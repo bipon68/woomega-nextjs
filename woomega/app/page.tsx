154af186a9bcddc1f911ecdcf4ffa9ec612c18bf
panthosarkar <pantho.sarkar.7@gmailcom>
2024-07-18 09:38:49 +0600

    inital project setup on next js and added required extensions

--- woomega/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Homepage from "@/woomega/program/Homepage";
-
-export default function Home() {
-  return <Homepage />;
-}
---
+++ woomega/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-[100vw] h-[100vh] bg-red-500"></div>
+  );
+}

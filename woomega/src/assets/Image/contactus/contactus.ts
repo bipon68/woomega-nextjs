0c4aa2c6061df75cefad8e7ef2b1712c043aae11
Anindo Roy <anindoroy112@gmail.com>
2024-07-30 10:40:44 +0600

    Contact us page done

---
+++ woomega/src/assets/Image/contactus/contactus.ts
@@ -0,0 +1,7 @@
+import banner from "./banner.png";
+import mapImg from "./map-marker.png";
+
+export const contactus = {
+  banner,
+  mapImg,
+};
--- woomega/src/woomega-nextjs/programs/components/Contact/ContactSection.tsx
+++ woomega/src/woomega-nextjs/programs/components/Contact/ContactSection.tsx
@@ -1,7 +1,22 @@
+import { contactus } from "@/assets/Image/contactus/contactus";
+import Image from "next/image";
 
 const ContactSection = () => {
   return (
-    <div> hello hello</div>
+    <div>
+      <div className=" h-[50vh] md:h-[60vh] ">
+        <div className=" absolute -z-10">
+          <Image
+            src={contactus.banner}
+            alt=""
+            className=" h-[50vh] md:h-[60vh] "
+          />
+        </div>
+        <div className="h-[60vh] flex items-center justify-center text-center text-5xl  z-10">
+          Contact Us
+        </div>
+      </div>
+    </div>
   );
 };
 

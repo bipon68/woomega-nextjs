c7e8e160c248572dbe4f3fe19c016950cf078703
Anindo Roy <anindoroy112@gmail.com>
2024-07-31 14:14:41 +0600

    submenu update

--- woomega/src/woomega-nextjs/features/navbar/Webmenu.tsx
+++ woomega/src/woomega-nextjs/features/navbar/Webmenu.tsx
@@ -1,44 +1,51 @@
 "use client";
 import { Logo } from "@/woomega-nextjs/libs/MainLogo";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { menu } from "./link";
 import Link from "next/link";
 
 const Webmenu = () => {
-  const [openDropdown, setOpenDropdown] = useState<string | null>(null);
+  const [hoveredItem, setHoveredItem] = useState<string | null>(null);
 
-  const toggleDropdown = (title: string) => {
-    if (openDropdown === title) {
-      setOpenDropdown(null);
-    } else {
-      setOpenDropdown(title);
-    }
+  const handleMouseEnter = (title: string) => {
+    setHoveredItem(title);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredItem(null);
   };
 
   return (
     <div className="h-full flex items-center gap-12">
       <div className="hidden md:block">
         <Link href={"/"}>
-          <Image src={Logo.MainLogo} alt="" />
+          <Image src={Logo.MainLogo} alt="Logo" />
         </Link>
       </div>
-      <div className="flex gap-9 ">
+      <div className="flex gap-9">
         {menu.map((item) => (
-          <div key={item.id} className="relative under-line">
+          <div
+            key={item.id}
+            className="relative under-line"
+            onMouseEnter={() => item.subMenu && handleMouseEnter(item.title)}
+            onMouseLeave={() => item.subMenu && handleMouseLeave()}
+            onClick={() => handleMouseLeave()}
+          >
             <Link href={item.path}>
-              <div
-                className="text-gray-700 hover:text-black px-3 py-2 text-sm font-medium"
-                onClick={() => item.subMenu && toggleDropdown(item.title)}
-              >
+              <div className="text-gray-700 hover:text-black px-3 my-2 text-sm font-medium cursor-pointer">
                 {item.title}
               </div>
             </Link>
-            {item.subMenu && openDropdown === item.title && (
-              <div className="absolute left-0 mt-2 w-48 bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+            {item.subMenu && (
+              <div
+                className={`absolute left-0 w-48 bg-white shadow-lg ring-1 ring-black ring-opacity-5 ${
+                  hoveredItem === item.title ? "block" : "hidden"
+                }`}
+              >
                 {item.subMenu.map((subItem) => (
                   <Link href={subItem.path} key={subItem.title}>
-                    <div className="block border-b px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                    <div className="block border-b px-5 py-3 text-sm text-gray-700 hover:bg-gray-100">
                       {subItem.title}
                     </div>
                   </Link>
